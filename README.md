12aad2e240a2e15fced821519b2d1cdbfed1090c
Fernando Fogaca <fernandopfogaca@gmail.com>
2024-05-04 18:45:47 +0200

    Inicia projeto  Node JS

---
+++ README.md
@@ -0,0 +1,17 @@
+# Busca SASS
+
+
+
+## Tecnologias
+- Node Js
+- HTML
+- NPM 
+- WEBPACK
+- JS
+- SASS
+
+
+## Como Iniciar o Projeto
+
+- Rodar o comando `npm install` para instalar as dependências
+- Rodar o comando `npm run start` para inicar o servidor de desenvolvimento.
---
+++ index.js
@@ -0,0 +1,13 @@
+
+import "./index.scss";
+
+document.getElementById("findBtn").addEventListener("click", iniciarExercicioFind);
+document.getElementById("filterBtn").addEventListener("click", iniciarExercicioFilter);
+
+function iniciarExercicioFilter() {
+ console.log("Iniciando exercício filter");   
+}
+function iniciarExercicioFind() {
+ console.log("Iniciando exercício find");   
+}
+
