950688c356b0e25ccfb103e4f78acd2f204b9b85
Fernando Fogaca <fernandopfogaca@gmail.com>
2024-05-05 21:59:38 +0200

    Final Updated

--- index.js
+++ index.js
@@ -4,10 +4,20 @@ import "./index.scss";
 document.getElementById("findBtn").addEventListener("click", iniciarExercicioFind);
 document.getElementById("filterBtn").addEventListener("click", iniciarExercicioFilter);
 
+const lista = document.getElementById("findList").value 
+const textoBusca = document.getElementById("findBusca").value
+
+console.log("Lista", lista);
+console.log("Texto de Busca:" , textoBusca);
+
+
+
+
+
 function iniciarExercicioFilter() {
- console.log("Iniciando exercício filter");   
+  
 }
 function iniciarExercicioFind() {
- console.log("Iniciando exercício find");   
+ 
 }
 
